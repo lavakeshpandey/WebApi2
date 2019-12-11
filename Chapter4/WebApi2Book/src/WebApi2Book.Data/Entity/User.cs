a8d11f2b82b2431b759e82bcb3fa05b07782dbed
Lavakesh Pandey <manjunatha.s3@Honeywell.com>
2019-12-11 19:16:52 +0530

    Initial commit

---
+++ Chapter4/WebApi2Book/src/WebApi2Book.Data/Entity/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Data.Entity
+{
+    public class User
+    {
+        public virtual long UserId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual byte[] Version { get; set; }
+
+    }
+}
---
+++ Chapter4/WebApi2Book/src/WebApi2BookDb/Tables/TaskUser.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TaskUser]
+(
+	[TaskId]	BIGINT		not null,
+	[UserId]	BIGINT		not null,
+	[ts]		ROWVERSION	not null,
+	PRIMARY KEY (TaskId, UserId),
+	FOREIGN KEY (UserId) REFERENCES dbo.[User] (UserId),
+	FOREIGN KEY (TaskId) REFERENCES dbo.Task   (TaskId)
+)
+GO
+
+CREATE INDEX ix_TaskUser_UserId ON TaskUser(UserId)
+GO
